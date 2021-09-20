cf9cd1edcf2378efb5019930292daeeaa2a65dbb
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-09-19 20:27:39 -0700

    finished typeDefs schema and started resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,27 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user){
+            const userData = await User.findOne({ _id: context.user._id }).select('-password');
+            return userData;
+            }
+            throw new AuthenticationError('Please login!')
+        },
+    },
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+            if (!user) throw new AuthenticationError('User not found')
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong Password!')
+            }
+            const token = signToken(user);
+            return {user, token};
+        },
+    },
+};
