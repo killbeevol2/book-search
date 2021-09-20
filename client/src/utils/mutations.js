03981e4310b4ed7a43a809a88aa0493caaca1865
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-09-19 21:29:45 -0700

    finished client side mutation for SAVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,8 +13,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $password: String!, $email: String!){
-        addUser(username: $username, password: $password, email: $email){
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
             token
             user {
                 _id
@@ -25,7 +25,23 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-    
+    mutation saveBook($bookData: BookInput!){
+        saveBook(bookData: $bookData){
+            user{
+                _id
+                username
+                email
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
